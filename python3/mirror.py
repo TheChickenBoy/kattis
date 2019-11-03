1652718bd3e260a583794755899e1330f37945df
Gustav Wass <c16gws@cs.umu.se>
2019-11-03 23:20:11 +0100

    Fix error

--- python3/mirror.py
+++ python3/mirror.py
@@ -1,16 +1,17 @@
 for q in range(int(input())):
     r,c = map(int,input().split())
     d = []
-    t = []
+    s=""
     for w in range(r):
         l = input()
         d.append(l)
-        t.append(l)
 
     for i in range(r):
         for j in range(c):
-            t[i][j] = d[r-1-i][c-1-j]
-    print(t)
+            s += d[r-1-i][c-1-j]
+        s+='\n'
+    print("Test " + str(q+1))
+    print(s)
 
 
 """
