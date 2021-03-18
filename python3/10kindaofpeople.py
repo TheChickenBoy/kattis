3cb7b4d9d2f97c61fcf673949750ef0d5b383361
chicken_ <gwass97@gmail.com>
2021-03-18 15:16:43 +0100

    added dict to keep track of updated value

--- python3/10kindaofpeople.py
+++ python3/10kindaofpeople.py
@@ -1,14 +1,14 @@
 import sys
 import heapq
 
-def floodfill(x, y,sp):
+def floodfill(x, y):
     unvisited = []
     heapq.heappush(unvisited, (x, y))
     while unvisited:
         pos = heapq.heappop(unvisited)
         x, y = pos[0], pos[1]
         if m[x][y] == sp:
-            m[x][y] = -1
+            m[x][y] = other
             if x > 0 and m[x-1][y] == sp:
                 heapq.heappush(unvisited, (x-1, y))
             if y > 0 and m[x][y-1] == sp:
@@ -18,20 +18,31 @@ def floodfill(x, y,sp):
             if x < r - 1 and m[x+1][y] == sp:
                 heapq.heappush(unvisited, (x+1, y))
 
+def check(x, y):
+    if x == y:
+        if d[x] == "1":
+            print("decimal")
+        else:
+            print("binary")
+    else:
+        print("neither")
+
 r, c = [int(x) for x in sys.stdin.readline().split()]
 m = [0]*r
 for i in range(r):
     l = list(sys.stdin.readline().strip())
     m[i] = l
-for _ in range(int(input())):
-    x1,y1,x2,y2 = [int(x) - 1 for x in sys.stdin.readline().split()]
+other = 0
+d = {}
+
+for _ in range(int(sys.stdin.readline())):
+    x1, y1, x2, y2 = [int(x) - 1 for x in sys.stdin.readline().split()]
     sp = m[x1][y1]
     gp = m[x2][y2]
-    if sp != gp:
-        print("neither")
-        continue
-    floodfill(x1, y1, sp)
-    if sp == "1":
-        print("decimal")
+    if type(sp) == int or type(gp) == int or sp != gp:
+        check(sp, gp)
     else:
-        print("binary")
+        other += 1
+        d[other] = sp
+        floodfill(x1, y1)
+        check(m[x1][y1], m[x2][y2])
